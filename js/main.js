282c5974ed874dd2a46eea9c00763b5b333e1c72
Mkhtr10 <abdelrhmanmkhtr10@gmail.com>
2025-09-02 18:49:58 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-// Smooth Inertia Scroll (Vanilla JS)
-(function () {
-    let targetScroll = window.pageYOffset; 
-    let currentScroll = window.pageYOffset; 
-    let ease = 0.05; // كل ما تقلل الرقم تبقى أبطأ
+// // Smooth Inertia Scroll (Vanilla JS)
+// (function () {
+//     let targetScroll = window.pageYOffset; 
+//     let currentScroll = window.pageYOffset; 
+//     let ease = 0.05; // كل ما تقلل الرقم تبقى أبطأ
 
-    function smoothScroll() {
-        currentScroll += (targetScroll - currentScroll) * ease;
-        window.scrollTo(0, currentScroll);
-        requestAnimationFrame(smoothScroll);
-    }
+//     function smoothScroll() {
+//         currentScroll += (targetScroll - currentScroll) * ease;
+//         window.scrollTo(0, currentScroll);
+//         requestAnimationFrame(smoothScroll);
+//     }
 
-    window.addEventListener(
-        "wheel",
-        function (e) {
-        e.preventDefault(); // منع السكرول العادي
-        targetScroll += e.deltaY; // حركة الماوس
+//     window.addEventListener(
+//         "wheel",
+//         function (e) {
+//         e.preventDefault(); // منع السكرول العادي
+//         targetScroll += e.deltaY; // حركة الماوس
 
-        // ممنوع يخرج برة حدود الصفحة
-        targetScroll = Math.max(
-            0,
-            Math.min(
-            targetScroll,
-            document.body.scrollHeight - window.innerHeight
-            )
-        );
-        },
-        { passive: false }
-    );
+//         // ممنوع يخرج برة حدود الصفحة
+//         targetScroll = Math.max(
+//             0,
+//             Math.min(
+//             targetScroll,
+//             document.body.scrollHeight - window.innerHeight
+//             )
+//         );
+//         },
+//         { passive: false }
+//     );
 
-    smoothScroll();
-})();
+//     smoothScroll();
+// })();
